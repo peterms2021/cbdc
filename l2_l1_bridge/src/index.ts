266e15e2cbc3bce0a0b3e5a19f4cd67bba6d008a
Peter Walker <pwalker@petersmsftmbp43.lan>
2023-04-20 18:15:17 -0400

    baseline for bridge calls

--- l2_l1_bridge/src/index.ts
+++ l2_l1_bridge/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
-import { setupConnection } from './Connect.js';
+import cors from "cors";
+import helmet from "helmet";
+import * as dotenv from "dotenv";
+
+import { setupConnection, contractInterface } from './Connect.js';
 import { eventListener } from './CbEvent.js';
 import { envRunMode, AppRunMode } from './Env.js';
+import { cbdcRouter } from 'src/transaction/Router.js'
+
 
 const app = express();
 const port = 21000;
 
-setupConnection();
+ setupConnection();
 let n  = envRunMode();
 console.log(` Root run mode: ${n}`);
 
@@ -15,12 +21,26 @@ if (n != AppRunMode.NORMAL_MODE as number) {
   eventListener();
 }
 else {
+  if (!process.env.CBDC_PORT) {
+    process.exit(1);
+  }
+ 
+ const PORT: number = parseInt(process.env.CBDC_PORT as string, 10);
+
+  app.use(helmet());   //see https://www.npmjs.com/package/helmet 
+  app.use(cors());    //see https://auth0.com/blog/cors-tutorial-a-guide-to-cross-origin-resource-sharing/
+  app.use(express.json());
+
+  app.use("/", cbdcRouter);
+
+  /*
   console.log("Normal app mode");
   app.get('/', (req, res) => {
     res.send('Hello World!');
   });
-
-  app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+  */
+ 
+  app.listen(PORT, () => {
+    return console.log(`CBDC Bridge Listening at http://localhost:${port}`);
   })
 }
