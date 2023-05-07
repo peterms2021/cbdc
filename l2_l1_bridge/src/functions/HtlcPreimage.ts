594115acf0f0883b30d69ed97da4a25c1e67eace
Peter Walker <pwalker@petersmsftmbp43.lan>
2023-05-07 17:37:32 -0400

    add more rest calls for testing

--- l2_l1_bridge/src/functions/HtlcPreimage.ts
+++ l2_l1_bridge/src/functions/HtlcPreimage.ts
@@ -1,37 +1,41 @@
-
 import { Contract } from "ethers";
 
-export async function HtlcPreimage( cbdc: Contract, htlc: string) : Promise<[result:string, err:string]>  {
-  let result:string ="";
-  let err:string = "";
- 
-  if(cbdc == undefined){
-    console.log("Cbdc object is not defined");
-    return[result,"No CBDC contract"];
-  } 
+export async function HtlcPreimage(
+  cbdc: Contract,
+  htlc: string
+): Promise<[result: string, err: string]> {
+  let result: string = "";
+  let err: string = "";
 
-  function setErr(val: string){
-    err =  val;
+  if (cbdc == undefined) {
+    console.log("Cbdc object is not defined");
+    return [result, "No CBDC contract"];
   }
-  function setResult(val: string){
-    result =  val;
+
+  function setErr(val: string) {
+    err = val;
   }
+  function setResult(val: string) {
+    result = val;
+  }
+
+  console.log(`HtlcPreimage: htlc:${htlc}`);
 
   async function handleSubmit() {
-          await cbdc
-          .htlcPreimage(htlc)
-          .then((result: string) => {
-            setResult(result);
-            //setErr(undefined);
-          })
-          .catch((err: Error) => {
-            //setResult(undefined);
-            setErr(err.message);
-          });
+    await cbdc
+      .htlcPreimage(htlc)
+      .then((result: string) => {
+        setResult(result);
+        //setErr(undefined);
+      })
+      .catch((err: Error) => {
+        //setResult(undefined);
+        setErr(err.message);
+      });
   }
 
   await handleSubmit();
-  return [result,err] ; 
-};
+  return [result, err];
+}
 
 export default HtlcPreimage;
